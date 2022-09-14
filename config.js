8e94689094b83c2b4014ca7004e5dde85aa4eb97
gvhfx <68278306+gvhfx@users.noreply.github.com>
2022-09-14 11:31:40 +0800

    Update config.js

--- config.js
+++ config.js
@@ -25,14 +25,16 @@ var config = {
      * Tip: 图片最好用正方形or接近正方形, 看起来效果更好
      */
     imgs: {
-        "心爱的小可爱": "./imgs/xiaokeai.png",
-        // "今天是你的生日": "./imgs/birthday.jpg",
+        "亲爱的女神": "./images/1.jpg",
+        "生日如约而至": "./images/2.jpg",
+        "比星光更灿烂": "./images/3.jpg",
+        "生日快乐宝贝！": "./images/4.jpg",
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
         turn_on: "开始",
         play: "音乐",
-        bannar_coming: "颜色",
+        bannar_coming: "还有",
         balloons_flying: "好像少点东西",
         cake_fadein: "蛋糕？",
         light_candle: "蜡烛？",
